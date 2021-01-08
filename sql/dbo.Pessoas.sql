0923b34dfdea4e5adbe16f8b28234a979f0f2638
bruno :) <brunolpw@gmail.com>
2021-01-08 16:13:17 -0300

    sql construção da tabela

---
+++ sql/dbo.Pessoas.sql
@@ -0,0 +1,9 @@
+﻿CREATE DATABASE BlazorApp;
+
+CREATE TABLE [dbo].[Pessoas] (
+    [Id]   INT           IDENTITY (1, 1) NOT NULL,
+    [Nome] VARCHAR (150) NOT NULL,
+    [Peso] INT           NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
